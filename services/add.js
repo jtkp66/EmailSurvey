f3a31ddee70a3e7f439a4dcdf6dd7487f6638a10
Jamie Pham <jtkp66@gmail.com>
2018-09-03 13:12:13 -0700

    Added billing and client side app

---
+++ services/add.js
@@ -0,0 +1,11 @@
+// write a function to retrieve a bblob of json
+// make an ajax request using fetch function
+
+// const fetchAlbums = async () => {
+//   const res = await fetch("http://rallycoding.herokuapp.com/api/music_albums");
+//   const json = await res.json();
+
+//   console.log(json);
+// };
+
+// fetchAlbums();
