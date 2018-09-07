b3dc5486b3287bf48e71860ce9f452d2bef46155
Jamie Pham <jtkp66@gmail.com>
2018-09-07 12:52:11 -0700

    Users can see survey results

--- services/add.js
+++ services/add.js
@@ -9,3 +9,14 @@
 // };
 
 // fetchAlbums();
+
+
+// Survey.updateOne({
+//     id: surveyId,
+//     recipients: {
+//         $elemMatch: { email: email, responded: false }
+//     }
+// }, {
+//     $inc: { [choice]: 1 },
+//     $set: { 'recipients.$.responded': true }
+// })
